c31095ee105770668953ee4811ff3e426d1ff2ca
dontroll17 <dontroll@yandex.ru>
2022-09-03 15:05:54 +0300

    feat: connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.CONNECT)],
   controllers: [AppController],
   providers: [AppService],
 })
