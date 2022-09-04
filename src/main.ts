3bd36bc69f397867790a8216fa00497cfd9ef0c7
dontroll17 <dontroll@yandex.ru>
2022-09-04 22:39:08 +0300

    fix: cors hot fix

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }});
 
   app.use(helmet());
-  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
